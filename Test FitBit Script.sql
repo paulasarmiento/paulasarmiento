cbd01a82d5619d74825969263f6e2821bb56c7b3
paulasarmiento <93755512+paulasarmiento@users.noreply.github.com>
2021-11-04 22:21:31 -0700

    Add files via upload

---
+++ Test FitBit Script.sql
@@ -0,0 +1,17 @@
+SELECT 
+    *
+FROM
+    Capstone.fitbit_dailyactivity_merged
+
+--
+
+SELECT *
+FROM
+    Capstone.fitbit_dailyactivity_merged
+WHERE ID = 1503960366
+
+--
+
+SELECT *
+FROM Capstone.fitbit_dailyactivity_merged
+WHERE TotalDistance > 5
